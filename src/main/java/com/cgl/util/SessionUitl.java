53f25d2e2357c530f06d7f21871829dec11b4772
acuteMouse <1012592349@qq.com>
2016-05-02 21:05:35 +0800

    把获取session的方法从BaseDaoImpl从提出来，放到单独的工具类中

--- src/main/java/com/cgl/util/SessionUitl.java
+++ src/main/java/com/cgl/util/SessionUitl.java
@@ -2,19 +2,21 @@ package com.cgl.util;
 
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
+import org.springframework.stereotype.Component;
 
 import javax.annotation.Resource;
 
 /**
- * Created cgl on 2016/3/22.
- * 类名：
- * 作用：
+ * Created by cgl on 2016/5/1.
+ * 类名：sessionUtil
+ * 作用:获取hibernateSession的工具类
  */
- public class SessionUitl {
-    @Resource
-    private static SessionFactory sessionFactory;
+@Component
+public class SessionUitl {
+    @Resource//注入sessionfactory
+    public SessionFactory sessionFactory;
 
-    public static Session getSeesion() {
-            return sessionFactory.getCurrentSession();
+    public Session getSeesion() { //返回session，由spring管理
+        return sessionFactory.getCurrentSession();
     }
 }
