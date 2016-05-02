70476b1c507c851a3293e7e5819c64f96775e2d3
acuteMouse <1012592349@qq.com>
2016-05-02 21:09:38 +0800

    部门管理

---
+++ src/main/java/com/cgl/service/IDepartmentService.java
@@ -0,0 +1,11 @@
+package com.cgl.service;
+
+import com.cgl.base.baseService.IBaseService;
+
+/**
+ * Created by cgl on 2016/4/04.
+ * 类名：
+ * 作用
+ */
+public interface IDepartmentService extends IBaseService{
+}
---
+++ src/main/java/com/cgl/serviceImpl/DepartmentServiceImpl.java
@@ -0,0 +1,14 @@
+package com.cgl.serviceImpl;
+
+import com.cgl.base.baseService.impl.BaseServiceImpl;
+import com.cgl.service.IDepartmentService;
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by cgl on 2016/4/04.
+ * 类名：
+ * 作用
+ */
+@Service("departmentService")
+public class DepartmentServiceImpl extends BaseServiceImpl implements IDepartmentService {
+}
